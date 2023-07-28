cc6bb8aa473751ebf6328480479d2e59fad70f5d
priya1128 <140085976+priya1128@users.noreply.github.com>
2023-07-28 12:10:03 +0530

    Update Sample.java

--- src/abstract_class/Sample.java
+++ src/abstract_class/Sample.java
@@ -1,1 +1,12 @@
 
+package abstract_class;
+
+public abstract class Sample {
+
+    final int a =10;
+    void display(){
+        System.out.println(a);
+    }
+    //abstract method cannot be private or final.
+    abstract void display1();
+}
