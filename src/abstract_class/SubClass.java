6bbddb8d1e3a219f807c1719963fe06f6ed720aa
priya1128 <140085976+priya1128@users.noreply.github.com>
2023-07-28 12:10:33 +0530

    Add files via upload

---
+++ src/abstract_class/SubClass.java
@@ -0,0 +1,15 @@
+package abstract_class;
+
+
+public class SubClass extends Sample {
+    public static void main(String[] arg) {
+        SubClass obj = new SubClass();
+        //obj.a = 90;
+        obj.display();
+    }
+
+    @Override
+    void display1() {
+
+    }
+}
